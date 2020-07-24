b2a44502c4416233e7bcaa4e365ab13c164a5120
shicheng.yu <shicheng.yu@fawaimanongdeMacBook-Pro.local>
2020-07-24 20:41:38 +0800

    2020年07月24日 20:38的快速提交

---
+++ configs/runtime.config.js
@@ -0,0 +1,9 @@
+
+
+module.exports = [{
+  name: "dva-app-project",
+  value: {
+    devDependencies: { "@cyber-tools/spa-cli": "*" },
+    remote: "https://github.com/cyber-worker/dva-spa-project"
+  }
+}];
